d750836859d80accb7b88291f5ca0df291be6df8
Finn Woelm <finn.woelm@unsdsn.org>
2020-10-02 11:48:23 +0200

    Fix tests: Update assertions to latest values
    Sometimes old data points get adjusted as more information becomes 
    available. This commit updates the test to match those updated data 
    points.

--- tests/jhu_deaths_test.py
+++ tests/jhu_deaths_test.py
@@ -4,8 +4,10 @@ from helpers.assert_value import assert_value
 # Load the indicator dataset
 dataset = load_indicator_to_test(__file__)
 
+
 def test_united_states():
-    assert_value(140756, 'USA', '2020-07-18', dataset)
+    assert_value(140775, "USA", "2020-07-18", dataset)
+
 
 def test_netherlands_aggregation():
-    assert_value(6076, 'NLD', '2020-06-13', dataset)
+    assert_value(6076, "NLD", "2020-06-13", dataset)
--- tests/marioli_effective_reproduction_rate_test.py
+++ tests/marioli_effective_reproduction_rate_test.py
@@ -4,8 +4,10 @@ from helpers.assert_value import assert_value
 # Load the indicator dataset
 dataset = load_indicator_to_test(__file__)
 
+
 def test_india():
-    assert_value(1.221, 'IND', '2020-06-30', dataset, decimals = 3)
+    assert_value(1.221, "IND", "2020-06-30", dataset, decimals=3)
+
 
 def test_united_states():
-    assert_value(1.213, 'USA', '2020-07-10', dataset, decimals = 3)
+    assert_value(1.211, "USA", "2020-07-10", dataset, decimals=3)
