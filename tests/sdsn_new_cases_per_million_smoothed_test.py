5dce07aaa0791d06bf24177943f3cb0aa9a61320
Finn Woelm <finn.woelm@unsdsn.org>
2020-10-09 10:58:39 +0200

    Add test for sdsn_new_cases_per_million_smoothed
    Add a new test for the sdsn_new_cases_per_million_smoothed indicator to 
    test that countries are assigned a value even for days with no original 
    data, as long as seven data points are present over the last fourteen 
    days.

--- tests/sdsn_new_cases_per_million_smoothed_test.py
+++ tests/sdsn_new_cases_per_million_smoothed_test.py
@@ -10,5 +10,13 @@ def test_belarus():
     assert_value(0.264, "BLR", "2020-03-13", dataset, decimals=3)
 
 
+# When less than 7 data points, no value should be reported
 def test_belarus_less_than_7_datapoints():
-    assert_missing("BLR", "2020-03-14", dataset)
+    assert_missing("BLR", "2020-03-15", dataset)
+
+
+# Even if a country has originally no data point for a given day,
+# a value should be reported if data is available for 7 of the last
+# 14 days.
+def test_belarus_no_original_datapoint():
+    assert_value(0.302, "BLR", "2020-03-14", dataset, decimals=3)
