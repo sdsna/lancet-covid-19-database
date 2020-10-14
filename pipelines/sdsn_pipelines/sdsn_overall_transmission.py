11f80818335f29161868f145945c6380dff3ea89
Finn Woelm <finn.woelm@unsdsn.org>
2020-10-14 18:39:18 +0200

    sdsn_overall_transmission: Carry forward latest positive rate for 7 days
    The positive test rate is not updated every day for some countries. To 
    cover these delays and avoid having missing classifications for 
    countries, we carry forward the latest positive rate for up to seven 
    days.

--- pipelines/sdsn_pipelines/sdsn_overall_transmission.py
+++ pipelines/sdsn_pipelines/sdsn_overall_transmission.py
@@ -1,5 +1,6 @@
 import os
 import pandas
+from time import localtime, strftime
 
 from config import INDICATOR_FOLDER
 from helpers.save_indicator import save_indicator
@@ -73,6 +74,39 @@ def run_pipeline(indicator):
         how="outer",
         on=["iso_code", "country", "date"],
     )
+
+    # Index on iso_code and date
+    dataset["date"] = pandas.to_datetime(dataset["date"])
+    dataset = dataset.set_index(["iso_code", "date"])
+
+    # Identify first and last date
+    today = strftime("%Y-%m-%d", localtime())
+    dates = dataset.index.get_level_values("date")
+    dates = dates.append(pandas.DatetimeIndex([pandas.to_datetime(today)]))
+
+    # Fill missing days in dataset
+    timerange = pandas.date_range(dates.min(), dates.max(), freq="D")
+    dataset = dataset.reindex(
+        pandas.MultiIndex.from_product(
+            [dataset.index.levels[0], timerange], names=["iso_code", "date"]
+        ),
+    )
+
+    # Carry-forward the latest positive test rate for each country for up to
+    # seven days
+    dataset["carried_positive_rate_smoothed"] = (
+        dataset.reset_index(level="iso_code")
+        .groupby("iso_code")["positive_rate_smoothed"]
+        .apply(lambda x: x.loc[x.last_valid_index() :].fillna(method="ffill", limit=7))
+    )
+
+    # Merge carried positive rate into the positive rate column
+    dataset["positive_rate_smoothed"] = dataset["positive_rate_smoothed"].combine_first(
+        dataset["carried_positive_rate_smoothed"]
+    )
+
+    # Reshape index back into columns
+    dataset.reset_index(inplace=True)
 
     # Generate classification
     dataset[indicator] = dataset.apply(generate_classification, axis=1)
